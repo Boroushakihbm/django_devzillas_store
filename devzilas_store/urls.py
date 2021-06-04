6d5c268814c7a942c3c87ec56fe36feb32cad809
Hashem <boroushakihbm@gmail.com>
2021-06-04 12:42:03 +0430

    prepare django rest framwork
    + create superuser
    +add restframwork
    +create app users

--- devzilas_store/urls.py
+++ devzilas_store/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from users.views import UserViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

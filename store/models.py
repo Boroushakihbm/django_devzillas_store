f5436e06426c14ec58d87c611f81291897a280a0
mrprocs <mrprocs@gmail.com>
2021-06-05 19:48:03 +0430

    Refactor code of department test module
    Refactoring code

--- store/models.py
+++ store/models.py
@@ -1,8 +1,5 @@
 from ._models.department import Department
 from ._models.feature import Feature
 
-__all__ = []
+__all__ = ['Department', 'Feature']
 
-__all__.append('Department')
-
-__all__.append('Feature')
